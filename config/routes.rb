2645b45b73405b434d99662aaf9648e742a0a59c
James <james.ryan.mcguire@gmail.com>
2021-09-22 14:47:31 -0400

    Adds seed data and routes for actors, users and shows

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  ## Actors
+
+  get "/actors" => "actors#index"
+  get "/actors/:id" => "actors#show"
+  post "/actors" => "actors#create"
+  delete "/actors/:id" => "actors#destroy"
+
+  ## Users
+
+  get "/users" => "users#index"
+  get "/users/:id" => "users#show"
+  post "/users" => "users#create"
+  delete "/users/:id" => "users#destroy"
+
+  ## Shows
+
+  get "/shows" => "shows#index"
+  get "/shows/:id" => "shows#show"
+  post "/shows" => "shows#create"
+  delete "/shows/:id" => "shows#destroy"
 end
