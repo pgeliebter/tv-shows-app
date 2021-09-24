138a7f93afba65b7ad9afd639f0dcb9c4099ce68
Pinny Geliebter <pgibralter@gmail.com>
2021-09-24 12:27:31 -0400

    adds bcrypt gem

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
     user = User.new(
       name: params[:name],
       email: params[:email],
+      password: params[:password],
       password_confirmation: params[:password_confirmation],
     )
     user.save
